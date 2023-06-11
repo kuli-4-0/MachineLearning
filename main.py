610e2e47854c3e9faac76458a7d201899dc092d0
alvian2022 <alviankhairi2019@gmail.com>
2023-06-11 15:08:28 +0700

    adding cors-flask

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import tensorflow_recommenders as tfrs
 import pandas as pd
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
+
 
 #Load model
 model = tf.keras.models.load_model('./content_model')
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
